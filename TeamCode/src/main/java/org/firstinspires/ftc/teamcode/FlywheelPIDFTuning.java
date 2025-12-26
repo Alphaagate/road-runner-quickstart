713bc60de196ed00a7128feec0d857e5d1fa790d
Austin Li <austin.jhl.awesome@gmail.com>
2025-12-26 14:15:31 -0800

    hardcode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FlywheelPIDFTuning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FlywheelPIDFTuning.java
@@ -1,15 +1,29 @@
 package org.firstinspires.ftc.teamcode;
-
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 
-import org.firstinspires.ftc.vision.VisionPortal;
-import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
-import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
-
-public class FlywheelPIDFTuning {
+//public class FlywheelPIDFTuning extends OpMode {
 //    private DcMotorEx outtakeMotor;
-//    private VisionPortal visionPortal;
-//    private AprilTagProcessor aprilTagProcessor;                 // Used for managing the AprilTag detection process.
-//    private boolean targetFound = false;    // Set to true when an AprilTag target is detected
-//    private AprilTagDetection detectedAprilTag;        // Used to hold the data for a detected AprilTag
-}
+//    public static final double NEW_P = 2.5;
+//    public static final double NEW_F = 0.5;
+//
+//    double highVelocity = 1100;
+//    double lowVelocity = 800;
+//    double curTargetVelocity = highVelocity;
+//
+//    double[] stepSizes = {10.0, 1.0, 0.1, 0.001, 0.0001};
+//
+//    int stepIndex = 1;
+//
+//    @Override
+//    public void init() {
+//        outtakeMotor = hardwareMap.get(DcMotorEx.class, "outtakemotor1");
+//        outtakeMotor.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+//        PIDFCoefficients pidfCoefficients = new PIDFCoefficients(NEW_P, 0, 0, F)
+//    }
+
+//}
