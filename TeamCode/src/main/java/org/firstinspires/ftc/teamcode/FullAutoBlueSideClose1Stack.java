7593b6d62d2b8016e87067376ac79d33932b3836
Austin Li <austin.jhl.awesome@gmail.com>
2026-01-04 19:34:08 -0800

    adjusting hood

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FullAutoBlueSideClose1Stack.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FullAutoBlueSideClose1Stack.java
@@ -8,13 +8,21 @@ import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 @Config
 @Autonomous(group = "Autonomous")
 public class FullAutoBlueSideClose1Stack extends AbstractFullAuto {
     protected ElapsedTime turretTimer = new ElapsedTime();
+    private static final double DESIRED_DISTANCE = 12.0;       //  this is how close the camera should get to the target (inches)
+    protected static final int DESIRED_TAG_ID = 24;       // Choose the tag you want to approach or set to -1 for ANY tag.
+    private static final int TURRET_GEAR_COUNT = 200;
+    private static final int TURRET_MOTOR_GEAR_COUNT = 50;
+    private static final double MAX_TURRET_TURN_POWER = 0.3;
+    private DcMotorEx turretMotor;
 
+    private double lastTargetPositionToMove = 0.0;
     @Override
     public Pose2d getInitialPose() {
         return new Pose2d(-58.3, -45, Math.toRadians(55));
