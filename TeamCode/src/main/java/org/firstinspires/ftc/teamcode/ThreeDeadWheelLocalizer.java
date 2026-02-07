e74a253fc09f39337c7a39222c026af267ba7639
Austin Li <austin.jhl.awesome@gmail.com>
2026-02-06 18:24:58 -0800

    untested, need to completely retune, remember to build at each step and also build from roadrunner quickstart values. 3 deadwheels

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ThreeDeadWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ThreeDeadWheelLocalizer.java
@@ -21,8 +21,8 @@ import org.firstinspires.ftc.teamcode.messages.ThreeDeadWheelInputsMessage;
 @Config
 public final class ThreeDeadWheelLocalizer implements Localizer {
     public static class Params {
-        public double par0YTicks =  -2367.4297784919431195621459801662;     // y position of the first parallel encoder (in tick units)
-        public double par1YTicks = 2407.2185142649169535043669210093;       // y position of the second parallel encoder (in tick units)
+        public double par0YTicks =  2367.4297784919431195621459801662;     // y position of the first parallel encoder (in tick units)
+        public double par1YTicks = -2407.2185142649169535043669210093;       // y position of the second parallel encoder (in tick units)
         public double perpXTicks = -3660.5636911135927226843265575678;      // x position of the perpendicular encoder (in tick units)
     }
 
@@ -40,8 +40,8 @@ public final class ThreeDeadWheelLocalizer implements Localizer {
         // TODO: make sure your config has **motors** with these names (or change them)
         //   the encoders should be plugged into the slot matching the named motor
         //   see https://ftc-docs.firstinspires.org/en/latest/hardware_and_software_configuration/configuring/index.html
-        par0 = new OverflowEncoder(new RawEncoder(hardwareMap.get(DcMotorEx.class, "backright")));
-        par1 = new OverflowEncoder(new RawEncoder(hardwareMap.get(DcMotorEx.class, "frontleft")));
+        par0 = new OverflowEncoder(new RawEncoder(hardwareMap.get(DcMotorEx.class, "frontleft")));
+        par1 = new OverflowEncoder(new RawEncoder(hardwareMap.get(DcMotorEx.class, "backright")));
         perp = new OverflowEncoder(new RawEncoder(hardwareMap.get(DcMotorEx.class, "frontright")));
 
         // TODO: reverse encoder directions if needed
